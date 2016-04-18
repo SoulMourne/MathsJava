5a8974a8f8c0f4a6492003d4b05255cd76504165
jgoodwin <jgoodwin@172.24.3.242>
2016-04-18 14:22:11 +0200

    Factorial calculation added

---
+++ src/count/Factorial.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package count;
+
+/**
+ *
+ * @author jgoodwin
+ */
+public class Factorial {
+    
+    /**
+     * Calculate the factorial of a number
+     * @param n the number we want to know the factorial
+     * @return the factorial of the number
+     */
+    public static double factorial(double n)
+    {
+        double res = 1;
+        for (int i = 1; i<= n; i++)
+        {
+            res *= i;
+        }
+        return res;
+    }    
+    
+}
